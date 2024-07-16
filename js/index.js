21f5c23914da92bbb9db945e930fd535642fa1f1
alskjd33 <alskjd33@gmail.com>
2024-07-16 18:08:38 +0900

    1920 완성

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-window.onload = function () {}
+window.onload = function () {
+    // 이벤트 스와이퍼
+    const swevent = new Swiper (".sw-event",{
+        navigation: {
+            nextEl: ".event .sw-next",
+            prevEl: ".event .sw-prev",
+        },
+            loop: true,
+            slidesPerView: 1,
+            grid: {
+              rows: 1,
+              fill: "row",
+            },
+
+        })
+    }
